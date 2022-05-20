620b0126b1f0c9284ff5b76b66e0f53d8528cc5f
Gitaaaaa <105896919+GitaNatalie@users.noreply.github.com>
2022-05-20 12:19:34 +0700

    First upload

---
+++ database.js
@@ -0,0 +1,114 @@
+let produk = [];
+
+let produk_detail = {
+  nama :"Jus",
+  kode : "BRG001", 
+  rating :"5.0",
+  stock: 10,
+  harga : 15000,
+  gambar: "https://awsimages.detik.net.id/community/media/visual/2016/09/12/554d4843-76c1-4650-9043-e17c6ac9e7c5.jpg?w=700&q=90"
+  };
+    produk.push(produk_detail);
+
+produk_detail = {
+    nama :"Bakso",
+    kode : "BRG002", 
+    rating :"5.0",
+    stock: 20,
+    harga : 15000,
+    gambar: "https://asset.kompas.com/crops/wlVZltJTRw6W2KeFLEVTLCV0fXw=/194x2:751x374/780x390/data/photo/2017/08/27/1140357898.jpg"
+    };
+    produk.push(produk_detail);
+
+
+produk_detail = {
+    nama :"Heci",
+    kode : "BRG003", 
+    rating :"5.0",
+    stock: 100,
+    harga : 2000,
+    gambar: "https://www.resepenyak.com/images/20201016120322-63-resep-heci-malang.jpg"
+    };
+    produk.push(produk_detail);
+
+produk_detail = {
+        nama :"Permen",
+        kode : "BRG004", 
+        rating :"5.0",
+        stock: 1000,
+        harga : 500,
+        gambar: "https://cdn1-production-images-kly.akamaized.net/aGsJSE-5PlZOIBar0T3L1ecKXRA=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2054451/original/070398300_1522833158-iStock-153502589.jpg"
+        };
+        produk.push(produk_detail);
+
+ produk_detail = {
+            nama :"Air Mineral",
+            kode : "BRG005", 
+            rating :"5.0",
+            stock: 50,
+            harga : 6000,
+            gambar: "https://res.cloudinary.com/dk0z4ums3/image/upload/v1589962541/attached_image/cara-memilih-air-kemasan-yang-baik.jpg"
+            };
+            produk.push(produk_detail);   
+
+produk_detail = {
+    nama : "Nextar",
+    kode : "nxt3", 
+    rating :4.8,
+    stock: 278,
+    harga : 2000,
+    gambar: "https://www.mirotakampus.com/resources/assets/images/product_images/1532142463.nextar%20fr.jpg"
+};
+    produk.push(produk_detail);   
+
+produk_detail = {
+        nama : "Tango",
+        kode : "To009", 
+        rating :4.9,
+        stock: 477,
+        harga : 10000,
+        gambar:"https://d21a1fytkm286.cloudfront.net/products_475834_image_B665_9714334.jpg"
+        };
+        produk.push(produk_detail);   
+
+produk_detail = {
+            nama : "Roti",
+            kode : "RT009", 
+            rating :4.9,
+            stock: 100,
+            harga : 15000,
+            gambar: "https://cdn-brilio-net.akamaized.net/news/2021/04/01/203113/1200x800-9-jenis-roti-tawar-populer-di-dunia-bikin-ngiler-210401r.jpg"
+            };
+            produk.push(produk_detail);   
+
+produk_detail = {
+          nama : "Soto",
+          kode : "To098", 
+          rating :4.9,
+          stock: 50,
+          harga : 9,
+          gambar : "https://awsimages.detik.net.id/community/media/visual/2021/12/14/resep-soto-ayam-jawa_43.jpeg?w=700&q=90"
+                };
+        produk.push(produk_detail);      
+        
+ produk_detail = {
+            nama : "Ayam Kremes",
+            kode : "A5560", 
+            rating :5.0,
+            stock: 30,
+            harga : 15000,
+            gambar : "https://www.topwisata.info/wp-content/uploads/2021/08/resep-ayam-goreng-kremes.webp"
+                  };
+          produk.push(produk_detail);      
+          
+produk_detail = {
+            nama : "Boba",
+            kode : "A5560", 
+            rating :5.0,
+            stock: 10,
+            harga : 5000,
+            gambar : "https://ik.imagekit.io/tvlk/cul-asset/guys1L+Yyer9kzI3sp-pb0CG1j2bhflZGFUZOoIf1YOBAm37kEUOKR41ieUZm7ZJ/cul-assets-252301483284-b172d73b6c43cddb/culinary/asset/COLL_shu-720x1092-FIT_AND_TRIM-5282ec529f7b2295ed418ca1f9259374.jpeg?tr=q-40,c-at_max,w-1440,h-2560&_src=imagekit"
+                  };
+          produk.push(produk_detail);      
+          
+console.log(produk);
